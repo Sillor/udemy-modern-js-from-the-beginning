3f39a5861bc0715b92fb30c6c70a81e10484059d
Egor <65542192+Sillor@users.noreply.github.com>
2023-11-02 17:33:03 -0700

    Done 01-dom-intro

---
+++ 06-document-object-model/01-dom-intro/script.js
@@ -0,0 +1,16 @@
+// console.log(window);
+// console.dir(window.document);
+
+// console.log(document.body.innerHTML);
+
+// console.log(document.links[0]);
+
+// document.body.innerHTML = "<h1>Hello World!</h1>";
+
+// document.write('<h1 style="color: green;">Hello World!</h1');
+
+console.log(document.getElementById("main"));
+const main = document.getElementById("main");
+// main.innerHTML = "Hello from main";
+
+document.querySelector("#main h1").innerText = "Hello!";
