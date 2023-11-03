dfefc96fa7793f6af60147117fc68ad030f0a423
Egor <65542192+Sillor@users.noreply.github.com>
2023-11-02 21:08:19 -0700

    Done 05-traversing-the-dom-elements

---
+++ 06-document-object-model/05-traversing-the-dom-elements/script.js
@@ -0,0 +1,31 @@
+// Get child elements from a parent
+let output;
+
+const parent = document.querySelector('.parent');
+output = parent.children;
+
+output = parent.children[1].innerText;
+output = parent.children[1].className;
+output = parent.children[1].nodeName;
+
+parent.children[1].innerText = 'Child Two';
+parent.children[1].style.color = 'red';
+
+parent.firstElementChild.innerText = 'Child One';
+parent.lastElementChild.innerText = 'Child Three';
+
+// Get parent elements from a child
+const child = document.querySelector('.child');
+output = child.parentElement;
+child.parentElement.style.border = '3px solid #ccc';
+child.parentElement.style.padding = '10px';
+
+// Sibling elements
+const secondItem = document.querySelector('.child:nth-child(2)');
+output = secondItem.innerText;
+output = secondItem.nextElementSibling.innerText;
+
+secondItem.nextElementSibling.style.color = 'green';
+secondItem.previousElementSibling.style.color = 'orange';
+
+console.log(output);
