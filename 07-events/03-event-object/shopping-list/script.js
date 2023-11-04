b281e488a843f78d1166c8e90ce64296f35b25cb
Egor <65542192+Sillor@users.noreply.github.com>
2023-11-03 22:10:11 -0700

    Done 03-event-object

--- 07-events/03-event-object/shopping-list/script.js
+++ 07-events/03-event-object/shopping-list/script.js
@@ -1,3 +1,35 @@
+const logo = document.querySelector('img');
+
+const onClick = (e) => {
+    // console.log(e.target);
+    // console.log(e.currentTarget);
+    // e.target.style.backgroundColor = 'black';
+    // console.log(e.type);
+    // console.log(e.timeStamp);
+    // console.log(e.clientX, e.clientY);
+    // console.log(e.offsetX, e.offsetY);
+    // console.log(e.pageX, e.pageY);
+    // console.log(e.screenX, e.screenY);
+    document.querySelector('h1').textContent = 'Shopping List';
+};
+
+const onDrag = (e) => {
+    document.querySelector('h1').textContent = `X ${e.clientX} Y ${e.clientY}`;
+};
+
+logo.addEventListener('click', onClick);
+logo.addEventListener('drag', onDrag);
+
+// document.body.addEventListener('click', (e) => {
+//     console.log(e.target);
+//     console.log(e.currentTarget);
+// });
+
+document.querySelector('a').addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('link was clicked');
+});
+
 /*
 - `target` - The element that triggered the event
 - `currentTarget` - The element that the event listener is attached to (These are the same in this case
