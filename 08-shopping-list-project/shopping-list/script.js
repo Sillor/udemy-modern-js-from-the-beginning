0e42f60d6881085fa6ee932b4eb53fe9de223bba
Egor <65542192+Sillor@users.noreply.github.com>
2023-11-05 03:58:34 -0800

    Add edit functionality to shopping-list

--- 08-shopping-list-project/shopping-list/script.js
+++ 08-shopping-list-project/shopping-list/script.js
@@ -3,6 +3,8 @@ const itemInput = document.querySelector('#item-input');
 const itemList = document.querySelector('#item-list');
 const clearBtn = document.querySelector('#clear');
 const itemFilter = document.querySelector('#filter');
+const formBtn = itemForm.querySelector('button');
+let isEditMode = false;
 
 function createButton(classes) {
     const button = document.createElement('button');
@@ -29,6 +31,16 @@ function addItem(e) {
     if (newItem === '') {
         alert('Please add an item');
         return;
+    }
+
+    // Check for edit mode
+    if (isEditMode) {
+        const itemToEdit = itemList.querySelector('.edit-mode');
+
+        removeItemFromStorage(itemToEdit.innerText);
+        itemToEdit.classList.remove('edit-mode');
+        itemToEdit.remove();
+        isEditMode = false;
     }
 
     addItemToDOM(newItem);
@@ -78,6 +90,22 @@ function displayItems() {
 function onClickItem(e) {
     if (e.target.parentElement.classList.contains('remove-item'))
         removeItem(e.target.parentElement.parentElement);
+    else {
+        setItemToEdit(e.target);
+    }
+}
+
+function setItemToEdit(item) {
+    isEditMode = true;
+
+    itemList
+        .querySelectorAll('li')
+        .forEach((i) => i.classList.remove('edit-mode'));
+
+    item.classList.add('edit-mode');
+    formBtn.innerHTML = '<i class="fa-solid fa-pen"></i> Update Item';
+    formBtn.style.backgroundColor = '#228B22';
+    itemInput.value = item.innerText;
 }
 
 function removeItem(item) {
@@ -117,6 +145,12 @@ function checkUI() {
         clearBtn.style.display = 'block';
         itemFilter.style.display = 'block';
     }
+
+    formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
+    formBtn.style.backgroundColor = '#333';
+    itemInput.value = '';
+
+    isEditMode = false;
 }
 
 function filterItems(e) {
