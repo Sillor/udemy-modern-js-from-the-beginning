89bc2f16a3b2f9af3799847fdd5a85939c67592b
Egor <65542192+Sillor@users.noreply.github.com>
2023-11-12 19:06:20 -0800

    Done 06-add-prototype-methods

--- 13-oop-constructors-prototypes/06-add-prototype-methods/script.js
+++ 13-oop-constructors-prototypes/06-add-prototype-methods/script.js
@@ -2,12 +2,30 @@ function Rectangle(name, width, height) {
   this.name = name;
   this.width = width;
   this.height = height;
-  this.area = function () {
-  	return this.width * this.height;
-  };
 }
 
+Rectangle.prototype.area = function () {
+  return this.width * this.height;
+};
+
+Rectangle.prototype.perimeter = function () {
+  return (this.width + this.height) * 2;
+};
+
+Rectangle.prototype.isSquare = function () {
+  return this.width === this.height;
+};
+
+Rectangle.prototype.changeName = function (newName) {
+  return (this.name = newName);
+};
 
 const rect = new Rectangle('Rect', 10, 10);
+const rect2 = new Rectangle('Rect2', 30, 40);
 
 console.log(rect);
+console.log(rect.area());
+console.log(rect.perimeter());
+console.log(rect.isSquare());
+console.log(rect.changeName('test'));
+console.log(rect2.area());
